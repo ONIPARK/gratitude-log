f4246b1aea93c6f8b257709c28a9c373b149b371
sionPark <onioni0000002@gmail.com>
2025-12-29 22:39:06 +0900

    Set up Spring Boot with MyBatis and PostgreSQL

---
+++ src/main/java/gratitude/gratitude_log/GratitudeLogApplication.java
@@ -0,0 +1,13 @@
+package gratitude.gratitude_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratitudeLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GratitudeLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gratitude/gratitude_log/GratitudeLogApplicationTests.java
@@ -0,0 +1,13 @@
+package gratitude.gratitude_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GratitudeLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
