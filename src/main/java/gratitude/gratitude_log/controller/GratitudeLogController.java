4ae144e3e0ce333d7b7a9e1b35ef57b4cd1eb061
sionPark <onioni0000002@gmail.com>
2025-12-31 09:55:12 +0900

    add Controller and Model

---
+++ src/main/java/gratitude/gratitude_log/controller/GratitudeLogController.java
@@ -0,0 +1,37 @@
+package gratitude.gratitude_log.controller;
+
+import gratitude.gratitude_log.dto.GratitudeLogCreateRequest;
+import gratitude.gratitude_log.service.GratitudeLogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/gratitude")
+public class GratitudeLogController {
+
+    private final GratitudeLogService service;
+
+    public GratitudeLogController(GratitudeLogService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/new")
+    public String crateForm(Model model) {
+        model.addAttribute("req", new GratitudeLogCreateRequest());
+        return "gratitude/create";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("req") GratitudeLogCreateRequest req, Model model) {
+        Long id = service.create(req);
+
+        // 保存完了メッセージ + フォーム初期化
+        model.addAttribute("saveId", id);
+        model.addAttribute("req", new GratitudeLogCreateRequest());
+        return "gratitude/create";
+    }
+}
--- src/main/java/gratitude/gratitude_log/service/GratitudeLogServiceImpl.java
+++ src/main/java/gratitude/gratitude_log/service/GratitudeLogServiceImpl.java
@@ -3,7 +3,11 @@ package gratitude.gratitude_log.service;
 import gratitude.gratitude_log.domain.GratitudeLog;
 import gratitude.gratitude_log.dto.GratitudeLogCreateRequest;
 import gratitude.gratitude_log.repository.GratitudeLogMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class GratitudeLogServiceImpl implements GratitudeLogService{
 
     private final GratitudeLogMapper mapper;
