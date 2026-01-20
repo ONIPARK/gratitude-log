e7ffc435eb2bf12506667926aea5be80d5e6b630
sionPark <onioni0000002@gmail.com>
2026-01-20 19:23:34 +0900

    chore: add DeleteApi.js

--- src/main/resources/js/api.js
+++ src/main/resources/js/api.js
@@ -1,1 +1,30 @@
-document.addEventListener()
+document.addEventListener("click", async function(e){
+    //
+    if (!e.target.classList.contains("dtn-delete")) return;
+
+    const id = e.target.dataset.id;
+    if (!confirm("削除しますか？")) return;
+
+    try {
+        const res = await fetch(`api/gratitude/${id}`, {
+            method: "DELETE",
+            headers: { "Accept" : "application/json" }
+        });
+
+        const data = await res.json().catch(() => null);
+
+        if (!res.ok) {
+            //alert(data?.message ?? `削除失敗 (status = ${res.status}`);
+            alert(data?.message ?? "削除対象を見つかりませんでした。");
+            return;
+        }
+
+        const li = e.target.closest("li");
+        if (li) li.remove();
+        alert(data?.message ?? "削除されました。");
+
+    } catch (err) {
+        alert("ネットワークエラーが発生しました。")
+    }
+
+});
