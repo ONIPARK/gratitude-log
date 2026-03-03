83e6d3010c975668dae5c701a66e6270d073b697
sionPark <onioni0000002@gmail.com>
2026-03-03 17:15:50 +0900

    feat: UserDetailsService連携によるログイン/ログアウトを実装

---
+++ src/main/java/gratitude/account/dto/AccountFindByEmailRequest.java
@@ -0,0 +1,14 @@
+package gratitude.account.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AccountFindByEmailRequest {
+    @NotBlank
+    @Email
+    private String email;
+}
